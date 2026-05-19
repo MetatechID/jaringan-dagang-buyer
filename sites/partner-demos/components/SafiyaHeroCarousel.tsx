fa3011ade81df8a65a9c27321408ab5cd09ab1dd
Listiarso Wastuargo <hallucinogenplus@gmail.com>
2026-05-19 21:27:38 +0700

    Change "Dimas ganteng" into "Hampers Ramadhan"
    Vibe-Tenant: safiyafood
    Vibe-By: hallucinogenplus@gmail.com

--- sites/partner-demos/components/SafiyaHeroCarousel.tsx
+++ sites/partner-demos/components/SafiyaHeroCarousel.tsx
@@ -23,7 +23,7 @@ const SLIDES: Slide[] = [
     eyebrow: "🌙 PROMO RAMADHAN 1447 H",
     title: (
       <>
-        Dimas ganteng,
+        Hampers Ramadhan,
         <br />
         <em style={{ fontStyle: "italic", fontWeight: 600 }}>
           tiga paket untuk berkah keluarga
